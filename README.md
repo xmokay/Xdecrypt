bf681be7a13e9d3284d71cea96fe5029e19ca2ee
xmokay <9464343+xmokay@users.noreply.github.com>
2025-04-07 17:39:26 +0800

    optimized

--- README.md
+++ README.md
@@ -9,15 +9,50 @@ pip3 install -r requirements.txt
 
 ## Usage
 ```
-usage: Xdecrypt.py [-h] [-s SID] [-p PASSWORD]
+usage: Xdecrypt.py [-h] [-d DIR] [-s SID] [-p PASSWORD] [-m MASTER]
 
 xsh, xfp password decrypt
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
+  -d DIR, --dir DIR     The directory where the .xsh file is stored
   -s SID, --sid SID     `username`+`sid`, user `whoami /user` in command.
   -p PASSWORD, --password PASSWORD
-                        the password in sessions or path of sessions
+                        the encrypted password to decrypt.
+  -m MASTER, --master MASTER
+                        master password
+```
+
+```
+$ python3 Xdecrypt.py
+Enter master password: xxxxxxxx
+================D:\Documents\NetSarang Computer\7\Xshell\Sessions\test.xsh=================
+Host:     172.0.0.1:22
+Username: root
+Password: 123
+```
+
+```
+$ python3 Xdecrypt.py -d "D:\Documents\NetSarang Computer"
+Enter master password: xxxxxxxx
+================D:\Documents\NetSarang Computer\7\Xshell\Sessions\test.xsh=================
+Host:     172.0.0.1:22
+Username: root
+Password: 123
+```
+
+```
+$ python3 Xdecrypt.py -m your_master_password
+================D:\Documents\NetSarang Computer\7\Xshell\Sessions\test.xsh=================
+Host:     172.0.0.1:22
+Username: root
+Password: 123
+```
+
+```
+$python Xdecrypt.py -p "BztDpmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM="
+Enter master password: xxxxxxxx
+123
 ```
 
 ```
--- Xdecrypt.py
+++ Xdecrypt.py
@@ -3,12 +3,39 @@ import sys
 import argparse
 import base64
 import configparser
+import ctypes
+import getpass
+from ctypes import wintypes
 
 from win32api import GetComputerName, GetUserName
 from win32security import LookupAccountName, ConvertSidToStringSid
 from Crypto.Hash import SHA256
 from Crypto.Cipher import ARC4
+def get_config_file_dir():
+    global args
+    if args.dir:
+        return args.dir
+    else:
+        # get documents dir
+        buf = ctypes.create_unicode_buffer(wintypes.MAX_PATH)
+        ctypes.windll.shell32.SHGetFolderPathW(None, 5, None, 0, buf)
+        # NetSarang Computer dir
+        ns_dir = os.path.join(buf.value, r"NetSarang Computer")
+        return ns_dir
 
+def get_master_password_list():
+    global args
+    lst = []
+    if args.master:
+        lst.append(args.master)
+    if args.sid:
+        lst.append(args.sid)
+    # 7
+    tmp = GetUserName()[::-1] + ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0])
+    lst.append(tmp[::-1])
+    # 6
+    lst.append(GetUserName() + ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0]))
+    return lst
 
 def decrypt_string(a1, a2):
     v1 = base64.b64decode(a2)
@@ -18,58 +45,64 @@ def decrypt_string(a1, a2):
     else:
         return None
 
-VERSION_6_CONFIG_PATH = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang Computer\6")
-VERSION_7_CONFIG_PATH = os.path.join(os.environ["USERPROFILE"], r"Documents\NetSarang Computer\7")
+def try_decrypt_string(password, master_password_lst):
+    global args
+    for master_password in master_password_lst:
+        r = decrypt_string(master_password, password)
+        if r:
+            return r
+    if not args.master:
+        args.master = getpass.getpass("Enter master password: ")
+        master_password_lst.insert(0, args.master)
+        r = decrypt_string(args.master, password)
+        if r:
+            return r
+    return None
 
-config_path = ""
-parser = argparse.ArgumentParser(description="xsh, xfp password decrypt")
-parser.add_argument("-s", "--sid", default="", type=str, help="`username`+`sid`, user `whoami /user` in command.")
-parser.add_argument("-p", "--password", default="", type=str, help="the password in sessions or path of sessions")
-args = parser.parse_args()
 
-if not args.password:
-    if os.path.exists(VERSION_6_CONFIG_PATH):
-        config_path = VERSION_6_CONFIG_PATH
-    elif os.path.exists(VERSION_7_CONFIG_PATH):
-        config_path = VERSION_7_CONFIG_PATH
-    else:
-        print("Error: can't found valid session path")
-        sys.exit(0)    
-    args.password = config_path
+def main():
+    global args
+    parser = argparse.ArgumentParser(description="xsh, xfp password decrypt")
+    parser.add_argument("-d", "--dir", default="", type=str, help="The directory where the .xsh file is stored")
+    parser.add_argument("-s", "--sid", default="", type=str, help="`username`+`sid`, user `whoami /user` in command.")
+    parser.add_argument("-p", "--password", default="", type=str, help="the encrypted password to decrypt.")
+    parser.add_argument("-m", "--master", default="", type=str, help="master password")
+    args = parser.parse_args()
 
-if not args.sid:
-    # method from https://github.com/JDArmy/SharpXDecrypt
-    if config_path == VERSION_7_CONFIG_PATH:
-        tmp = GetUserName()[::-1] + ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0])
-        args.sid = tmp[::-1]
-    else:
-        args.sid = GetUserName() + ConvertSidToStringSid(LookupAccountName(GetComputerName(), GetUserName())[0])
+    master_password_lst = get_master_password_list()
+    if args.password:
+        dec = try_decrypt_string(args.password, master_password_lst)
+        if dec:
+            print(dec)
+            sys.exit(0)
+        else:
+            print("decryption failed")
+            sys.exit(1)
 
-if not os.path.isdir(args.password):
-    r = decrypt_string(args.sid, args.password)
-    if r:
-        print(r)
+    ns_dir = get_config_file_dir()
+    for root, dirs, files in os.walk(ns_dir):
+        for f in files:
+            if f.endswith(".xsh") or f.endswith(".xfp"):
+                filepath = os.path.join(root, f)
+                cfg = configparser.ConfigParser()
+                try:
+                    cfg.read(filepath)
+                except UnicodeDecodeError:
+                    cfg.read(filepath, encoding="utf-16")
 
-for root, dirs, files in os.walk(args.password):
-    for f in files:
-        if f.endswith(".xsh") or f.endswith(".xfp"):
-            filepath = os.path.join(root, f)
-            cfg = configparser.ConfigParser()
-            try:
-                cfg.read(filepath)
-            except UnicodeDecodeError:
-                cfg.read(filepath, encoding="utf-16")
+                try:
+                    if f.endswith(".xsh"):
+                        host = "{}:{}".format(cfg["CONNECTION"]["Host"], cfg["CONNECTION"]["Port"])
+                        username = cfg["CONNECTION:AUTHENTICATION"]["UserName"]
+                        password = try_decrypt_string(cfg["CONNECTION:AUTHENTICATION"]["Password"], master_password_lst)
+                    else:
+                        host = "{}:{}".format(cfg["Connection"]["Host"], cfg["Connection"]["Port"])
+                        username = cfg["Connection"]["UserName"]
+                        password = try_decrypt_string(cfg["Connection"]["Password"], master_password_lst)
+                    print(
+                        f"{filepath:=^100}\nHost:     {host}\nUsername: {username}\nPassword: {password}")
+                except Exception as e:
+                    print(f"{filepath:=^100}\nError:{e}")
 
-            try:
-                if f.endswith(".xsh"):
-                    host = "{}:{}".format(cfg["CONNECTION"]["Host"], cfg["CONNECTION"]["Port"])
-                    username = cfg["CONNECTION:AUTHENTICATION"]["UserName"]
-                    password = decrypt_string(args.sid, cfg["CONNECTION:AUTHENTICATION"]["Password"])
-                else:
-                    host = "{}:{}".format(cfg["Connection"]["Host"], cfg["Connection"]["Port"])
-                    username = cfg["Connection"]["UserName"]
-                    password = decrypt_string(args.sid, cfg["Connection"]["Password"])
-                print(
-                    f"{filepath:=^100}\nHost:     {host}\nUsername: {username}\nPassword: {password}")
-            except Exception as e:
-                print(f"{filepath:=^100}\nError:{e}")
+if __name__ == "__main__":
+    main()
